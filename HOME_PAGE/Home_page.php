07ade4428232d27a83af8b891ac236d0f0fc4c37
jsjs2324 <salgado.julia.hernandez@gmail.com>
2024-11-17 20:07:38 +0800

    hgjhgHG
    HJGJHG

--- HOME_PAGE/Home_page.php
+++ HOME_PAGE/Home_page.php
@@ -82,6 +82,7 @@ if (isset($_POST['update'])) {
   } else {
     echo "Error updating patient record: " . mysqli_error($con);
   }
+  
 }
 
 ?>
@@ -93,7 +94,7 @@ if (isset($_POST['update'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="Home_Page.css">
+  <link rel="stylesheet" href="Home_page.css">
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
     integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
